389f4922c2e5ed9bc3a8aef96364b35001288776
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-22 15:17:51 -0600

    Clase 20: migraciones

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Entities;
 
 namespace DataAccess
@@ -11,10 +12,17 @@ namespace DataAccess
     public class InventaryContext : DbContext
     {
         public DbSet<ProductEntity> Products { get; set; }
-        public DbSet<WareHouseEntity> WhereHouses { get; set; }
+        public DbSet<WareHouseEntity> WareHouses { get; set; }
         public DbSet<CategoryEntity> Categories { get; set; }
         public DbSet<InputOutputEntity> InOut { get; set; }
         public DbSet<StorageEntity> Storages { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=(Local);Database=nventoryDBTesting;User Id=TestingUserDb; Password=TestingUserDb1");
+            }
+        }
     }
 }
---
+++ DataAccess/Migrations/20210522211600_FirstMigration.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WareHouses",
+                columns: table => new
+                {
+                    WareHouseId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    WareHouseName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    WareHouseAddress = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WareHouses", x => x.WareHouseId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    ProductName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(600)", maxLength: 600, nullable: true),
+                    TotalQuantity = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storages",
+                columns: table => new
+                {
+                    StorageId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    dateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PartialQuantity = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    WareHouseId = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storages", x => x.StorageId);
+                    table.ForeignKey(
+                        name: "FK_Storages_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Storages_WareHouses_WareHouseId",
+                        column: x => x.WareHouseId,
+                        principalTable: "WareHouses",
+                        principalColumn: "WareHouseId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InOut",
+                columns: table => new
+                {
+                    InputOutputId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    InOutDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    IsInput = table.Column<bool>(type: "bit", nullable: false),
+                    StorageId = table.Column<string>(type: "nvarchar(50)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InOut", x => x.InputOutputId);
+                    table.ForeignKey(
+                        name: "FK_InOut_Storages_StorageId",
+                        column: x => x.StorageId,
+                        principalTable: "Storages",
+                        principalColumn: "StorageId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InOut_StorageId",
+                table: "InOut",
+                column: "StorageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storages_ProductId",
+                table: "Storages",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storages_WareHouseId",
+                table: "Storages",
+                column: "WareHouseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InOut");
+
+            migrationBuilder.DropTable(
+                name: "Storages");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "WareHouses");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
