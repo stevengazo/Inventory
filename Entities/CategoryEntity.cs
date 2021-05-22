9f1633038b439c67afbea87b874745455f30d731
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2021-05-22 11:30:33 -0600

    Clase 16: adicion de modelo de datos en nuevo proyecto

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entities
+{
+    public class CategoryEntity
+    {
+        [Key]       
+        public string CategoryId { set; get;}
+
+        public string CategoryName { get; set; }
+       
+        
+    }
+}
